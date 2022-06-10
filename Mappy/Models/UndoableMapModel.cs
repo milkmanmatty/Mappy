ce0419b0e3083ae5bf7195580cb074eac963b4ee
Matt <matt@milkmanmatty.com.au>
2022-06-10 22:56:33 +1000

    Uncomment a line that was accidentally left commented out.

--- Mappy/Models/UndoableMapModel.cs
+++ Mappy/Models/UndoableMapModel.cs
@@ -663,7 +663,7 @@
                     return false;
                 }
 
-                var isBlocked = // !coordSet.Contains(translatedPoint) &&
+                var isBlocked = !coordSet.Contains(translatedPoint) &&
                     this.HasFeatureInstanceAt(translatedPoint.X, translatedPoint.Y);
                 if (isBlocked)
                 {
