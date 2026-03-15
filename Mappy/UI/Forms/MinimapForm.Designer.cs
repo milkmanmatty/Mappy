32f5b6f3b5b733033be7750ab498768a576881c3
btigi <btigi@users.noreply.github.com>
2026-03-15 08:21:06 +0000

    Don't allow the minimap form to be maximized

--- Mappy/UI/Forms/MinimapForm.Designer.cs
+++ Mappy/UI/Forms/MinimapForm.Designer.cs
@@ -30,36 +30,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.minimapControl = new MinimapControl();
-            this.SuspendLayout();
-            // 
-            // minimapControl
-            // 
-            this.minimapControl.Location = new System.Drawing.Point(0, 0);
-            this.minimapControl.Margin = new System.Windows.Forms.Padding(0);
-            this.minimapControl.Name = "minimapControl";
-            this.minimapControl.RectColor = System.Drawing.Color.Yellow;
-            this.minimapControl.Size = new System.Drawing.Size(252, 252);
-            this.minimapControl.TabIndex = 1;
-            this.minimapControl.Text = "minimapControl1";
-            this.minimapControl.ViewportRect = new System.Drawing.Rectangle(5, 5, 50, 50);
-            this.minimapControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MinimapControl1MouseDown);
-            this.minimapControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MinimapControl1MouseMove);
-            this.minimapControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MinimapControl1MouseUp);
-            // 
-            // MinimapForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoSize = true;
-            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(252, 252);
-            this.Controls.Add(this.minimapControl);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
-            this.Name = "MinimapForm";
-            this.Text = "Minimap";
-            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MinimapFormFormClosing);
-            this.ResumeLayout(false);
+			this.minimapControl = new Mappy.UI.Controls.MinimapControl();
+			this.SuspendLayout();
+			// 
+			// minimapControl
+			// 
+			this.minimapControl.Location = new System.Drawing.Point(0, 0);
+			this.minimapControl.Margin = new System.Windows.Forms.Padding(0);
+			this.minimapControl.Name = "minimapControl";
+			this.minimapControl.RectColor = System.Drawing.Color.Yellow;
+			this.minimapControl.Size = new System.Drawing.Size(252, 252);
+			this.minimapControl.TabIndex = 1;
+			this.minimapControl.Text = "minimapControl1";
+			this.minimapControl.ViewportRect = new System.Drawing.Rectangle(5, 5, 50, 50);
+			this.minimapControl.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MinimapControl1MouseDown);
+			this.minimapControl.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MinimapControl1MouseMove);
+			this.minimapControl.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MinimapControl1MouseUp);
+			// 
+			// MinimapForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.AutoSize = true;
+			this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+			this.ClientSize = new System.Drawing.Size(252, 252);
+			this.Controls.Add(this.minimapControl);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+			this.MaximizeBox = false;
+			this.Name = "MinimapForm";
+			this.Text = "Minimap";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MinimapFormFormClosing);
+			this.ResumeLayout(false);
 
         }
 
