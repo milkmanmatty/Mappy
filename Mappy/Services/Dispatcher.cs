f3005c78c8dd215a95a544420f15a30b71bc5d88
Matty <matt@whateverweb.com.au>
2026-03-19 21:06:39 +1000

    Vertical and Horizontal Flip working with undo capability. Does not cook the bitmap cache either.

--- Mappy/Services/Dispatcher.cs
+++ Mappy/Services/Dispatcher.cs
@@ -423,19 +423,19 @@ namespace Mappy.Services
                         return;
                     }
 
+                    if (!Directory.Exists(TempDir))
+                    {
+                        Directory.CreateDirectory(TempDir);
+                    }
+
                     IMapTile floatTile = map.FloatingTiles[map.SelectedTile.Value].Item;
 
                     MapTile destTile = new MapTile(floatTile.TileGrid.Width, floatTile.TileGrid.Height);
                     GridMethods.Copy(floatTile.TileGrid, destTile.TileGrid, 0, 0, 0, 0, floatTile.TileGrid.Width, floatTile.TileGrid.Height);
                     GridMethods.Copy(floatTile.HeightGrid, destTile.HeightGrid, 0, 0, 0, 0, floatTile.HeightGrid.Width, floatTile.HeightGrid.Height);
 
-                    GridMethods.FlipArea(floatTile.TileGrid, destTile.TileGrid, floatTile.TileGrid.Width, floatTile.TileGrid.Height, direction);
-                    GridMethods.FlipArea(floatTile.HeightGrid, destTile.HeightGrid, floatTile.HeightGrid.Width, floatTile.HeightGrid.Height, direction);
-
-                    if (!Directory.Exists(TempDir))
-                    {
-                        Directory.CreateDirectory(TempDir);
-                    }
+                    GridMethods.FlipArea(floatTile.TileGrid, destTile.TileGrid, 0, 0, 0, 0, floatTile.TileGrid.Width, floatTile.TileGrid.Height, FlipDirection.Horizontal);
+                    GridMethods.FlipArea(floatTile.HeightGrid, destTile.HeightGrid, 0, 0, 0, 0, floatTile.HeightGrid.Width, floatTile.HeightGrid.Height, FlipDirection.Horizontal);
 
                     this.imageExportingService.ExportSection(
                         destTile,
