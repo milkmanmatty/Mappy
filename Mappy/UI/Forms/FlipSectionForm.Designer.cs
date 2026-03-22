05a96b0bcee71aad6bdc548cedae010a77f0bedb
Matty <matt@whateverweb.com.au>
2026-03-22 22:12:36 +1000

    Tweak Flip dialog

--- Mappy/UI/Forms/FlipSectionForm.Designer.cs
+++ Mappy/UI/Forms/FlipSectionForm.Designer.cs
@@ -51,20 +51,21 @@ namespace Mappy.UI.Forms
             // 
             // label1
             // 
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(12, 128);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(449, 64);
+            this.label1.Size = new System.Drawing.Size(491, 75);
             this.label1.TabIndex = 1;
             this.label1.Text = "Which direction do you want to flip the selected section?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // flipDirectionGroupBox
             // 
             this.flipDirectionGroupBox.Controls.Add(this.flipVertRadio);
             this.flipDirectionGroupBox.Controls.Add(this.flipHoriRadio);
-            this.flipDirectionGroupBox.Location = new System.Drawing.Point(12, 195);
+            this.flipDirectionGroupBox.Location = new System.Drawing.Point(12, 206);
             this.flipDirectionGroupBox.Name = "flipDirectionGroupBox";
-            this.flipDirectionGroupBox.Size = new System.Drawing.Size(448, 113);
+            this.flipDirectionGroupBox.Size = new System.Drawing.Size(491, 113);
             this.flipDirectionGroupBox.TabIndex = 2;
             this.flipDirectionGroupBox.TabStop = false;
             this.flipDirectionGroupBox.Text = "Flip direction";
@@ -73,7 +74,7 @@ namespace Mappy.UI.Forms
             // 
             this.flipVertRadio.Location = new System.Drawing.Point(9, 66);
             this.flipVertRadio.Name = "flipVertRadio";
-            this.flipVertRadio.Size = new System.Drawing.Size(427, 32);
+            this.flipVertRadio.Size = new System.Drawing.Size(476, 32);
             this.flipVertRadio.TabIndex = 1;
             this.flipVertRadio.Text = "Flip Vertically";
             this.flipVertRadio.UseVisualStyleBackColor = true;
@@ -83,7 +84,7 @@ namespace Mappy.UI.Forms
             this.flipHoriRadio.Checked = true;
             this.flipHoriRadio.Location = new System.Drawing.Point(9, 30);
             this.flipHoriRadio.Name = "flipHoriRadio";
-            this.flipHoriRadio.Size = new System.Drawing.Size(427, 27);
+            this.flipHoriRadio.Size = new System.Drawing.Size(476, 27);
             this.flipHoriRadio.TabIndex = 0;
             this.flipHoriRadio.TabStop = true;
             this.flipHoriRadio.Text = "Flip Horizontally";
@@ -93,9 +94,9 @@ namespace Mappy.UI.Forms
             // 
             this.RelightGroupBox.Controls.Add(this.doNotRelightRadio);
             this.RelightGroupBox.Controls.Add(this.relightRadio);
-            this.RelightGroupBox.Location = new System.Drawing.Point(12, 376);
+            this.RelightGroupBox.Location = new System.Drawing.Point(12, 393);
             this.RelightGroupBox.Name = "RelightGroupBox";
-            this.RelightGroupBox.Size = new System.Drawing.Size(448, 110);
+            this.RelightGroupBox.Size = new System.Drawing.Size(491, 110);
             this.RelightGroupBox.TabIndex = 3;
             this.RelightGroupBox.TabStop = false;
             this.RelightGroupBox.Text = "Relight terrain?";
@@ -104,7 +105,7 @@ namespace Mappy.UI.Forms
             // 
             this.doNotRelightRadio.Location = new System.Drawing.Point(9, 68);
             this.doNotRelightRadio.Name = "doNotRelightRadio";
-            this.doNotRelightRadio.Size = new System.Drawing.Size(427, 28);
+            this.doNotRelightRadio.Size = new System.Drawing.Size(476, 28);
             this.doNotRelightRadio.TabIndex = 1;
             this.doNotRelightRadio.Text = "Do not relight terrain shadows";
             this.doNotRelightRadio.UseVisualStyleBackColor = true;
@@ -114,7 +115,7 @@ namespace Mappy.UI.Forms
             this.relightRadio.Checked = true;
             this.relightRadio.Location = new System.Drawing.Point(9, 25);
             this.relightRadio.Name = "relightRadio";
-            this.relightRadio.Size = new System.Drawing.Size(427, 37);
+            this.relightRadio.Size = new System.Drawing.Size(476, 37);
             this.relightRadio.TabIndex = 0;
             this.relightRadio.TabStop = true;
             this.relightRadio.Text = "Relight terrain shadows";
@@ -122,28 +123,29 @@ namespace Mappy.UI.Forms
             // 
             // label2
             // 
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 318);
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 329);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(449, 35);
+            this.label2.Size = new System.Drawing.Size(492, 41);
             this.label2.TabIndex = 4;
             this.label2.Text = "Relight terrain shadows?\r\n\r\n";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // label3
             // 
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 353);
+            this.label3.Location = new System.Drawing.Point(12, 370);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(446, 20);
+            this.label3.Size = new System.Drawing.Size(489, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "This will not change shadows cast from one object to another";
             // 
             // button1
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.button1.Location = new System.Drawing.Point(12, 492);
+            this.button1.Location = new System.Drawing.Point(12, 509);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(448, 47);
+            this.button1.Size = new System.Drawing.Size(491, 47);
             this.button1.TabIndex = 6;
             this.button1.Text = "Confess your sins to Venom";
             this.button1.UseVisualStyleBackColor = true;
@@ -151,17 +153,18 @@ namespace Mappy.UI.Forms
             // warningImg
             // 
             this.warningImg.Image = global::Mappy.Properties.Resources.Warning;
-            this.warningImg.Location = new System.Drawing.Point(12, 44);
+            this.warningImg.Location = new System.Drawing.Point(12, 29);
             this.warningImg.Name = "warningImg";
-            this.warningImg.Size = new System.Drawing.Size(48, 48);
+            this.warningImg.Size = new System.Drawing.Size(72, 72);
+            this.warningImg.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.warningImg.TabIndex = 7;
             this.warningImg.TabStop = false;
             // 
             // warningLabel
             // 
-            this.warningLabel.Location = new System.Drawing.Point(75, 18);
+            this.warningLabel.Location = new System.Drawing.Point(90, 18);
             this.warningLabel.Name = "warningLabel";
-            this.warningLabel.Size = new System.Drawing.Size(372, 110);
+            this.warningLabel.Size = new System.Drawing.Size(413, 110);
             this.warningLabel.TabIndex = 8;
             this.warningLabel.Text = resources.GetString("warningLabel.Text");
             // 
@@ -169,7 +172,7 @@ namespace Mappy.UI.Forms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(470, 549);
+            this.ClientSize = new System.Drawing.Size(515, 567);
             this.Controls.Add(this.warningLabel);
             this.Controls.Add(this.warningImg);
             this.Controls.Add(this.button1);
@@ -178,6 +181,7 @@ namespace Mappy.UI.Forms
             this.Controls.Add(this.RelightGroupBox);
             this.Controls.Add(this.flipDirectionGroupBox);
             this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FlipSectionForm";
